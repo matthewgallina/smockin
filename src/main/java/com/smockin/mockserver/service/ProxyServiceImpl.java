feb613c7eaea4b9eca9cf42575be2fa2f4c6f78d
Matthew Gallina <Matthew.Gallina@passivsystems.com>
2017-08-11 14:52:53 +0100

    Fixed concurrency issue with proxy response engine. Unit tests around this all work. However the general design could do with reworking.

--- src/main/java/com/smockin/mockserver/service/ProxyServiceImpl.java
+++ src/main/java/com/smockin/mockserver/service/ProxyServiceImpl.java
@@ -21,50 +21,74 @@ public class ProxyServiceImpl implements ProxyService {
 
     private final Logger logger = LoggerFactory.getLogger(ProxyServiceImpl.class);
 
-    private Map<ProxiedKey, List<ProxiedDTO>> responsesQueueMap = new HashMap<ProxiedKey, List<ProxiedDTO>>();
+    private Map<ProxiedKey, List<ProxiedDTO>> synchronizedProxyResponsesMap = new HashMap<ProxiedKey, List<ProxiedDTO>>();
 
-    private final int signalPermitCount = 1;
-    private final Semaphore signal = new Semaphore(0);
-    private final Semaphore lock = new Semaphore(1);
+    private final int SIGNAL_PERMIT_COUNT = 1;
+    private final int MAX_TIMEOUT_MILLIS = 30000;
+
+    private final Semaphore signal = new Semaphore(0, true);
+    private final Semaphore lock = new Semaphore(1, true);
 
     @Override
     public RestfulResponse waitForResponse(final RestfulMock mock) {
 
-        final long timeOut = (mock.getProxyTimeOutInMillis() > 0)?mock.getProxyTimeOutInMillis():Long.MAX_VALUE;
+        final long timeOut = (mock.getProxyTimeOutInMillis() > 0)?mock.getProxyTimeOutInMillis():MAX_TIMEOUT_MILLIS;
 
         try {
 
             while (true) {
 
                 // Checks for any existing permits, if none are found then waits (for a set amount of time) until one is available.
-                boolean foundOne = signal.tryAcquire(signalPermitCount, timeOut, TimeUnit.MILLISECONDS);
+                boolean foundOne = signal.tryAcquire(SIGNAL_PERMIT_COUNT, timeOut, TimeUnit.MILLISECONDS);
 
                 if (!foundOne) {
-                    // timed out
-                    return null;
+
+                    // The wait has timed out
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("The wait for '" + mock.getMethod() + " " + mock.getPath() + "' has timed out");
+                        logger.debug("Will attempt 1 more look up, in case ");
+                    }
+
+                    // HACK to fix the design issue.
+                    // We use the timeout as a way of checking again for a relevant response.
+                    return lookUpResponse(mock);
                 }
 
-                // Acquire lock before checking queue
-                lock.acquire();
+                final RestfulResponse restfulResponse = lookUpResponse(mock);
 
-                final List<ProxiedDTO> responses = responsesQueueMap.get(new ProxiedKey(mock.getPath(), mock.getMethod()));
+                if (restfulResponse == null) {
+                    // Design flaw!
 
-                if (responses == null || responses.isEmpty()) {
+                    /*
+                        You are using a semaphore as part of a consumer / producer model which is like a Queue.
 
-                    // no matching path was found, so release lock and continue waiting
-                    lock.release();
+                        What you really need is MQ 'Topic' type behaviour, given the consumer is looking for a
+                        'particular' RestfulResponse object.
 
+
+                        The problem is to do with signalling alignment and described below :
+
+                        Consumer 'Thread A' is waiting for a permit and wants the value '1'.
+
+                        Consumer 'Thread B' is waiting for a permit and wants the value '2'.
+
+                        Producer 'Thread C' posts the value '2' and stores this (in the synchronizedProxyResponsesMap).
+
+                        Consumer 'Thread A' acquires a permit, but cannot find the value '1' and so continues (in the loop) waiting on another permit.
+
+                        Producer 'Thread D' posts the value '1'.
+
+                        Consumer 'Thread B' acquires a permit and finds the value '1', it exits and loop and returns a response.
+
+                        Consumer 'Thread A' is now stuck waiting on a permit, despite value '2', being in the synchronizedProxyResponsesMap.
+
+
+                        Using a topic approach would fix this as 'all' of the consumers would be notified each time a producer stores a value.
+                    */
                     continue;
-                } else {
-
-                    // A matching path was found, so consume/remove the element from the queue, release the lock and return.
-                    final ProxiedDTO proxiedResponse = responses.remove(0);
-
-                    lock.release();
-
-                    return new RestfulResponse(proxiedResponse.getHttpStatusCode(), proxiedResponse.getResponseContentType(), proxiedResponse.getBody(), new HashSet<Map.Entry<String, String>>());
                 }
-
+                
+                return restfulResponse;
             }
 
         } catch (InterruptedException ex) {
@@ -79,17 +103,44 @@ public class ProxyServiceImpl implements ProxyService {
 
         try {
             lock.acquire();
-            final List<ProxiedDTO> responses = responsesQueueMap.getOrDefault(dto.getPath(), new ArrayList<ProxiedDTO>());
+            final ProxiedKey key = new ProxiedKey(dto.getPath(), dto.getMethod());
+            final List<ProxiedDTO> responses = synchronizedProxyResponsesMap.getOrDefault(key, new ArrayList<ProxiedDTO>());
             responses.add(dto);
-            responsesQueueMap.put(new ProxiedKey(dto.getPath(), dto.getMethod()), responses); // not sure we need this line...
+            synchronizedProxyResponsesMap.put(key, responses); // not sure we need this line...
             lock.release();
 
-            signal.release(signalPermitCount);
+            if (logger.isDebugEnabled())
+                logger.debug("Added dto " + dto.getPath() + ". Responses size is " + responses.size());
 
-        } catch (InterruptedException ex) {
+            signal.release(SIGNAL_PERMIT_COUNT);
+
+        } catch (Throwable ex) {
             logger.error("Error whilst adding proxied response to queue", ex);
         }
 
+    }
+
+    RestfulResponse lookUpResponse(final RestfulMock mock) throws InterruptedException {
+
+        // Acquire lock before checking queue
+        lock.acquire();
+
+        final List<ProxiedDTO> responses = synchronizedProxyResponsesMap.get(new ProxiedKey(mock.getPath(), mock.getMethod()));
+
+        if (responses == null || responses.isEmpty()) {
+
+            // no matching path was found, so release lock and continue waiting
+            lock.release();
+
+            return null;
+        }
+
+        // A matching path was found, so consume/remove the element from the queue, release the lock and return response.
+        final ProxiedDTO proxiedResponse = responses.remove(0);
+
+        lock.release();
+
+        return new RestfulResponse(proxiedResponse.getHttpStatusCode(), proxiedResponse.getResponseContentType(), proxiedResponse.getBody(), new HashSet<Map.Entry<String, String>>());
     }
 
 }
---
+++ src/test/java/com/smockin/mockserver/service/ProxyServiceVolumeTest.java
@@ -0,0 +1,165 @@
+package com.smockin.mockserver.service;
+
+import com.smockin.admin.persistence.entity.RestfulMock;
+import com.smockin.admin.persistence.enums.MockTypeEnum;
+import com.smockin.admin.persistence.enums.RecordStatusEnum;
+import com.smockin.admin.persistence.enums.RestMethodEnum;
+import com.smockin.mockserver.service.bean.ProxiedKey;
+import com.smockin.mockserver.service.dto.ProxiedDTO;
+import com.smockin.mockserver.service.dto.RestfulResponse;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * Created by mgallina on 11/08/17.
+ */
+public class ProxyServiceVolumeTest {
+
+    private ProxyService proxyService;
+    private final int proxiedTestCount = 1000;
+
+    private ExecutorService executor;
+    private Runnable[] producers;
+    private TestCallable[] consumers;
+    private ProxiedKey[] keys;
+
+    @Before
+    public void setUp() {
+
+        executor = Executors.newFixedThreadPool(100);
+
+        proxyService = new ProxyServiceImpl();
+
+        keys = generateKeys();
+
+        producers = new Runnable[proxiedTestCount];
+
+        for (int p=0; p < proxiedTestCount; p++) {
+
+            final ProxiedKey pk = keys[p];
+
+            producers[p] = new Runnable() {
+                @Override
+                public void run() {
+                    proxyService.addResponse(new ProxiedDTO(pk.getPath(), pk.getMethod(), 200, MediaType.APPLICATION_JSON_VALUE, "{ \"path\" : \"" + pk.getPath() + "\" }"));
+                }
+            };
+
+        }
+
+        consumers = new TestCallable[proxiedTestCount];
+
+        for (int c=0; c < proxiedTestCount; c++) {
+
+            final ProxiedKey pk = keys[c];
+
+            consumers[c] = new TestCallable() {
+
+                public ProxiedKey getProxiedKey() {
+                     return pk;
+                }
+
+                @Override
+                public Object call() {
+                    return proxyService.waitForResponse(new RestfulMock(pk.getPath(), pk.getMethod(), RecordStatusEnum.ACTIVE, MockTypeEnum.PROXY, 3000));
+                }
+            };
+
+        }
+
+    }
+
+    @Test
+    public void proxyConcurrency_itemAlreadyInQueue_Test() throws InterruptedException, ExecutionException, TimeoutException {
+
+        // Test
+        List<FutureAssertionWrapper> futureAssertionsList = new ArrayList<FutureAssertionWrapper>();
+
+        for (int i=0; i < proxiedTestCount; i++) {
+            final TestCallable callable = consumers[i];
+            futureAssertionsList.add(new FutureAssertionWrapper(callable.getProxiedKey(), executor.submit(callable)));
+            executor.submit(producers[i]);
+        }
+
+        // Assertions
+        int assertionsCount = 0;
+
+        for (FutureAssertionWrapper faw : futureAssertionsList) {
+
+            final ProxiedKey pk = faw.getProxiedKey();
+            final Future future = faw.getFuture();
+
+            do {
+
+                final Object response = future.get();
+
+                Assert.assertTrue(response instanceof RestfulResponse);
+                final RestfulResponse restfulResponse = (RestfulResponse)response;
+
+                Assert.assertEquals(200, restfulResponse.getHttpStatusCode());
+                Assert.assertEquals(MediaType.APPLICATION_JSON_VALUE, restfulResponse.getResponseContentType());
+                Assert.assertNotNull(restfulResponse.getResponseBody());
+                Assert.assertTrue(restfulResponse.getResponseBody().contains(pk.getPath()));
+                Assert.assertTrue(restfulResponse.getHeaders().isEmpty());
+
+                assertionsCount++;
+            } while (!future.isDone());
+
+        }
+
+        Assert.assertEquals(proxiedTestCount, assertionsCount);
+
+    }
+
+    private ProxiedKey[] generateKeys() {
+
+        final ProxiedKey[] keys = new ProxiedKey[proxiedTestCount];
+
+        for (int p=0; p < proxiedTestCount; p++) {
+
+            final int distinctAppender;
+
+            if (p % 3 == 1) {
+                distinctAppender = 1;
+            } else if (p % 2 == 1) {
+                distinctAppender = 2;
+            } else {
+                distinctAppender = 3;
+            }
+
+            keys[p] = new ProxiedKey("/helloworld" + distinctAppender, RestMethodEnum.GET);
+        }
+
+        return keys;
+    }
+
+    interface TestCallable extends Callable {
+        ProxiedKey getProxiedKey();
+    }
+
+    class FutureAssertionWrapper {
+
+        private final ProxiedKey proxiedKey;
+        private final Future future;
+
+        public FutureAssertionWrapper(ProxiedKey proxiedKey, Future future) {
+            this.proxiedKey = proxiedKey;
+            this.future = future;
+        }
+
+        public ProxiedKey getProxiedKey() {
+            return proxiedKey;
+        }
+        public Future getFuture() {
+            return future;
+        }
+
+    }
+
+}
