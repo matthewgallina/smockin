d462dfe17839243cee0302b0f20034712d80d6af
Matthew Gallina <Matthew.Gallina@passivsystems.com>
2017-08-15 17:13:41 +0100

    Added more content to help section. All Javascript and CSS dependancies are now local.

--- src/main/resources/public/js/controller/help_section.js
+++ src/main/resources/public/js/controller/help_section.js
@@ -1,5 +1,5 @@
 
-app.controller('helpSectionController', function($scope, $location) {
+app.controller('helpSectionController', function($scope, $location, $uibModal) {
 
     //
     // Labels
@@ -8,5 +8,42 @@ app.controller('helpSectionController', function($scope, $location) {
 
     $scope.inboundParamsInResponseTitle = 'Including inbound parameters in your response';
     $scope.mockTypesSectionTitle = 'Return types explained';
+
+    // Functions
+    $scope.openImageViewer = function(url) {
+
+        var modalInstance = $uibModal.open({
+          templateUrl: 'help/help_image_viewer.html',
+          controller: 'helpImageViewerController',
+          resolve: {
+            data: function () {
+              return {
+                "url" : url
+              };
+            }
+          }
+        });
+
+    };
+
+});
+
+app.controller('helpImageViewerController', function($scope, $uibModalInstance, data) {
+
+    //
+    // Labels
+    $scope.closeButtonLabel = 'Close';
+
+
+    //
+    // Data Objects
+    $scope.imagePath = data.url;
+
+
+    //
+    // Functions
+    $scope.doClose = function() {
+        $uibModalInstance.close();
+    };
 
 });
