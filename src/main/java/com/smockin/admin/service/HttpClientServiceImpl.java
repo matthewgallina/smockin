6ac694a3998040daeff605cdda0382c8adebff57
Matthew Gallina <Matthew.Gallina@passivsystems.com>
2017-08-16 15:22:42 +0100

    Added screenshots examples for all current help guides. Also fixed issue with posting form data from http client.

--- src/main/java/com/smockin/admin/service/HttpClientServiceImpl.java
+++ src/main/java/com/smockin/admin/service/HttpClientServiceImpl.java
@@ -9,15 +9,23 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -79,16 +87,18 @@ public class HttpClientServiceImpl implements HttpClientService {
 
     HttpClientResponseDTO post(final HttpClientCallDTO reqDto) throws IOException {
 
-        final Request request = Request.Post(reqDto.getUrl())
-                .bodyByteArray((reqDto.getBody() != null)?reqDto.getBody().getBytes():null);
+        final Request request = Request.Post(reqDto.getUrl());
+
+        handleRequestData(request, reqDto.getHeaders(), reqDto);
 
         return executeRequest(request, reqDto.getHeaders());
     }
 
     HttpClientResponseDTO put(final HttpClientCallDTO reqDto) throws IOException {
 
-        final Request request = Request.Put(reqDto.getUrl())
-                .bodyByteArray((reqDto.getBody() != null)?reqDto.getBody().getBytes():null);
+        final Request request = Request.Put(reqDto.getUrl());
+
+        handleRequestData(request, reqDto.getHeaders(), reqDto);
 
         return executeRequest(request, reqDto.getHeaders());
     }
@@ -166,6 +176,39 @@ public class HttpClientServiceImpl implements HttpClientService {
                 extractResponseHeaders(httpResponse),
                 extractResponseBody(httpResponse)
         );
+    }
+
+    void handleRequestData(final Request request, final Map<String, String> requestHeaders, final HttpClientCallDTO reqDto) {
+
+        if (requestHeaders.containsValue(MediaType.APPLICATION_FORM_URLENCODED_VALUE)) {
+
+            final List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+
+            if (reqDto.getBody() != null && reqDto.getBody().contains("&")) {
+
+                final String[] formParameterPairsArray = reqDto.getBody().split("&");
+
+                for (String pairStr : formParameterPairsArray) {
+
+                    if (pairStr.contains("=")) {
+
+                        final String[] pairArray = pairStr.split("=");
+
+                        if (pairArray.length == 2) {
+                            postParameters.add(new BasicNameValuePair(pairArray[0], pairArray[1]));
+                        }
+
+                    }
+
+                }
+
+            }
+
+            request.bodyForm(postParameters);
+            return;
+        }
+
+        request.bodyByteArray((reqDto.getBody() != null)?reqDto.getBody().getBytes():null);
     }
 
     private void debugDTO(final HttpClientCallDTO dto) {
