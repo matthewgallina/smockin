86756ada79487395bd5c53916f281bce6d2622f9
Matthew Gallina <m_gallina@yahoo.com>
2018-10-30 08:10:40 +0000

    Excluded new websocket package from context path in integeration tests, as there is no Servletcontext candidate in test scope.

--- src/test/java/com/smockin/SmockinTestConfig.java
+++ src/test/java/com/smockin/SmockinTestConfig.java
@@ -2,12 +2,14 @@ package com.smockin;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by mgallina.
  */
-@ComponentScan({ "com.smockin.admin", "com.smockin.mockserver" })
+@ComponentScan(value = { "com.smockin.admin", "com.smockin.mockserver" },
+                excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.smockin.admin.websocket.*"))
 @EnableJpaRepositories("com.smockin.admin.persistence.dao")
 @EntityScan("com.smockin.admin.persistence.entity")
 public class SmockinTestConfig {
