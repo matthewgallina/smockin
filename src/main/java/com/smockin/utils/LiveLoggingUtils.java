9402e948d705a821676de4094dc87780a0f66673
Matthew Gallina <m_gallina@yahoo.com>
2018-11-14 18:19:20 +0000

    Readability improvements to log file based live logging.

--- src/main/java/com/smockin/utils/LiveLoggingUtils.java
+++ src/main/java/com/smockin/utils/LiveLoggingUtils.java
@@ -15,8 +15,8 @@ import java.util.stream.Collectors;
  */
 public final class LiveLoggingUtils {
 
-    private static final String INBOUND_ARROW = "--------------------->";
-    private static final String OUTBOUND_ARROW = "<---------------------";
+    private static final String INBOUND_ARROW = "------>";
+    private static final String OUTBOUND_ARROW = "<------";
     private static final String NOT_AVAILABLE = "n/a";
     private static final String CARRIAGE_RET = "\n";
     private static final String HEADER_DELIMITER = ",";
@@ -27,67 +27,102 @@ public final class LiveLoggingUtils {
         return String.valueOf("t-" + atomicTraceIdInt.getAndIncrement());
     }
 
+    /**
+        REQUEST ------> PROXY SERVER ------> MOCK SERVER
+        GET http://localhost:9000/proxypass
+        trace id: t-6
+        headers:
+        body: n/a
+    */
     public static String buildLiveLogInboundFileEntry(final String reqId, final String method, final String url, final String contentType, final Map<String, String> headers, final String reqBody, final boolean viaProxy) {
-
-        final StringBuilder sb = buildLiveLogHeader(LiveLoggingDirectionEnum.REQUEST, reqId, (viaProxy) ? "(proxy)" : null);
-
-        sb.append("method: ");
-        sb.append(method);
-        sb.append(CARRIAGE_RET);
-        sb.append("url: ");
-        sb.append(url);
-        sb.append(CARRIAGE_RET);
-        sb.append("content type: ");
-        sb.append(StringUtils.defaultIfBlank(contentType, NOT_AVAILABLE));
-        sb.append(CARRIAGE_RET);
-        sb.append("headers: ");
-        sb.append(headers.entrySet().stream().map(h -> h.getKey() + "=" + h.getValue()).collect(Collectors.joining(HEADER_DELIMITER)));
-        sb.append(CARRIAGE_RET);
-        sb.append("body: ");
-        sb.append(StringUtils.defaultIfBlank(reqBody, NOT_AVAILABLE));
-        sb.append(CARRIAGE_RET);
-
-        return sb.toString();
-    }
-
-    public static String buildLiveLogOutboundFileEntry(final String reqId, final int status, final String contentType, final Map<String, String> headers, final String responseBody, final boolean viaProxy, final boolean isProxyMockedResponse) {
-
-        final StringBuilder sb = buildLiveLogHeader(LiveLoggingDirectionEnum.RESPONSE, reqId, ((viaProxy) ? "(proxy <- " + ((isProxyMockedResponse) ? "mocked" : "original") + ")" : null));
-
-        sb.append("status: ");
-        sb.append(status);
-        sb.append(CARRIAGE_RET);
-        sb.append("content type: ");
-        sb.append(StringUtils.defaultIfBlank(contentType, NOT_AVAILABLE));
-        sb.append(CARRIAGE_RET);
-        sb.append("headers: ");
-        sb.append(headers.entrySet().stream().map(h -> h.getKey() + "=" + h.getValue()).collect(Collectors.joining(HEADER_DELIMITER)));
-        sb.append(CARRIAGE_RET);
-        sb.append("body: ");
-        sb.append(StringUtils.defaultIfBlank(responseBody, NOT_AVAILABLE));
-        sb.append(CARRIAGE_RET);
-
-        return sb.toString();
-    }
-
-    private static StringBuilder buildLiveLogHeader(final LiveLoggingDirectionEnum direction, final String reqId, final String directionMeta) {
 
         final StringBuilder sb = new StringBuilder();
 
-        sb.append(getDirectionArrow(direction));
+        sb.append(buildLiveLogFileHeader(LiveLoggingDirectionEnum.REQUEST, viaProxy, null));
+        sb.append(method)
+            .append(" ")
+            .append(url)
+            .append(CARRIAGE_RET);
+        sb.append("trace id:")
+            .append(reqId)
+            .append(CARRIAGE_RET);
+        sb.append("headers: ")
+            .append(headers.entrySet().stream().map(h -> h.getKey() + "=" + h.getValue()).collect(Collectors.joining(HEADER_DELIMITER)))
+            .append(CARRIAGE_RET);
+        sb.append("body: ")
+            .append(StringUtils.defaultIfBlank(reqBody, NOT_AVAILABLE))
+            .append(CARRIAGE_RET);
 
-        if (directionMeta != null) {
-            sb.append(" ");
-            sb.append(directionMeta);
+        return sb.toString();
+    }
+
+    /**
+         200 RESPONSE <------ PROXY SERVER <------ MOCK SERVER
+         trace id: t-6
+         headers:
+         body: { "name" : "jane" }
+     */
+    public static String buildLiveLogOutboundFileEntry(final String reqId, final int status, final String contentType, final Map<String, String> headers, final String responseBody, final boolean viaProxy, final boolean isProxyMockedResponse) {
+
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(status)
+                .append(" ")
+                .append(buildLiveLogFileHeader(LiveLoggingDirectionEnum.RESPONSE, viaProxy, isProxyMockedResponse));
+        sb.append("trace id:")
+                .append(reqId)
+                .append(CARRIAGE_RET);
+        sb.append("headers: ")
+                .append(headers.entrySet().stream().map(h -> h.getKey() + "=" + h.getValue()).collect(Collectors.joining(HEADER_DELIMITER)))
+                .append(CARRIAGE_RET);
+        sb.append("body: ")
+            .append(StringUtils.defaultIfBlank(responseBody, NOT_AVAILABLE))
+            .append(CARRIAGE_RET);
+
+        return sb.toString();
+    }
+
+    /**
+     *
+     * e.g
+     * REQUEST ------> PROXY SERVER ------> MOCK SERVER
+     * RESPONSE <------  PROXY SERVER <------ MOCK SERVER
+     *
+     * REQUEST ------> PROXY SERVER ------> ORIGINAL DESTINATION
+     * RESPONSE <------  PROXY SERVER <------ ORIGINAL DESTINATION
+     *
+     * REQUEST ------> MOCK SERVER
+     * RESPONSE <------  MOCK SERVER
+     *
+     * @param direction
+     * @param viaProxy
+     *
+     * @returns String
+     */
+    private static String buildLiveLogFileHeader(final LiveLoggingDirectionEnum direction, final boolean viaProxy, final Boolean mockedResponse) {
+
+        final String directionalArrow = getDirectionalArrow(direction);
+
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(direction.name());
+        sb.append(" ");
+
+        sb.append(directionalArrow);
+
+        if (viaProxy) {
+            sb.append(" PROXY SERVER ");
+            sb.append(directionalArrow);
         }
 
+        if (viaProxy && mockedResponse != null && !mockedResponse) {
+            sb.append(" ORIGINAL DESTINATION");
+        } else {
+            sb.append(" MOCK SERVER");
+        }
         sb.append(CARRIAGE_RET);
 
-        sb.append("id: ");
-        sb.append(reqId);
-        sb.append(CARRIAGE_RET);
-
-        return sb;
+        return sb.toString();
     }
 
     public static LiveLoggingDTO buildLiveLogInboundDTO(final String reqId, final String method, final String url, final String contentType, final Map<String, String> headers, final String reqBody, final boolean viaProxy) {
@@ -100,13 +135,13 @@ public final class LiveLoggingUtils {
         return new LiveLoggingDTO(reqId, LiveLoggingDirectionEnum.RESPONSE, viaProxy, new LiveLoggingOutboundContentDTO(StringUtils.defaultIfBlank(contentType, NOT_AVAILABLE), headers, StringUtils.defaultIfBlank(responseBody, NOT_AVAILABLE), status, isProxyMockedResponse));
     }
 
-    private static String getDirectionArrow(final LiveLoggingDirectionEnum direction) {
+    private static String getDirectionalArrow(final LiveLoggingDirectionEnum direction) {
 
         switch (direction) {
             case REQUEST:
-                return direction.name() + " " + INBOUND_ARROW;
+                return INBOUND_ARROW;
             case RESPONSE:
-                return OUTBOUND_ARROW + " " + direction.name();
+                return OUTBOUND_ARROW;
             default:
                 throw new IllegalArgumentException("Unsupported Live Logging Direction: " + direction);
         }
