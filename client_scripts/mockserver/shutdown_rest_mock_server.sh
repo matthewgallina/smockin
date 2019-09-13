bd2a8b9beeda61c9f3b63aa9f639acbb2f354a4a
Matthew Gallina <m_gallina@yahoo.com>
2019-09-13 11:24:15 +0100

    Removed old utility scripts.

--- client_scripts/mockserver/shutdown_rest_mock_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PORT=8000
-BASE_URL="http://localhost:$PORT"
-
-curl -i -X POST -H "Content-Type: application/json" -d '{}' $BASE_URL/mockedserver/rest/stop
-
-exit 0
--- client_scripts/mockserver/start_rest_mock_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PORT=8000
-BASE_URL="http://localhost:$PORT"
-
-curl -i -H "Content-Type: application/json" -X POST -d '{}' "$BASE_URL/mockedserver/rest/start"
-
-exit 0
